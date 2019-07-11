e9f3978fdb3dae137077463be54268ddb2798bdb
adam-weiler <adam.weiler@yahoo.ca>
2019-07-11 15:19:19 -0400

    Exercise 5 complete.

--- exercises.py
+++ exercises.py
@@ -1,7 +1,7 @@
 #Exercise 0
 
 #Lists
-fav_colours = ['blue', 'black', 'red', 'orange']
+fav_colours = ['Blue', 'Black', 'Red', 'Orange']
 family_ages = [32, 38, 37, 30, 64, 63]
 coin_flips = ['heads', 'tails', 'heads', 'heads', 'tails']
 fav_artists = ['Nightwish', 'Pink Floyd', 'Hawkwind', 'Kamelot', 'Tool']
@@ -10,7 +10,7 @@ fav_artists = ['Nightwish', 'Pink Floyd', 'Hawkwind', 'Kamelot', 'Tool']
 unique_words = { 'polypod': 'a many-legged animal', 'gamophobia': 'the fear of marriage', 'logodaedalus': 'an artificer in words' }
 fav_movies = { 'Ex Machina': 2014, 'Back to the Future':1985, 'Predestination':2014 }
 pop_cities = { 'Boston': 685100, 'Toronto':2930000, 'Halifax':431500, 'Hamburg':1810000 }
-sibling_ages = { 'Lucas':38, 'Erin':37, 'Kathleen':30, 'Justin':30, 'Patrick':31 }
+sibling_ages = { 'Lucas':38, 'Erin':37, 'Kathleen':30, 'Jodi':64, 'Dan':63, 'Justin':30, 'Patrick':31 }
 
 
 #Exercise 1
@@ -25,8 +25,11 @@ print (fav_movies['Predestination']) # 2014
 #Exercise 2
 print('\nExercise #2')
 print (fav_colours[-1]) # orange
+
 pop_cities['San Francisco'] = 884400 #Adds a new city to the dictionary.
+
 coin_flips.reverse() #Reverses the order of the list.
+
 print(pop_cities['Toronto']) # 2930000
 
 for artist in fav_artists:
@@ -53,6 +56,27 @@ for age in family_ages:
         print(age) #Prints five times; each age less than 40.
 
 print(max(family_ages)) # 64
+
 coin_flips.count('heads') # 3
+
 fav_artists.pop(3) #Removes 'Kamelot' from the list.
+
 pop_cities['Toronto'] = 101101101 #Changes the value in the dictionary.
+
+
+#Exercise 5
+print('\nExercise #5')
+print(sum(pop_cities.values())) # 104912101
+
+for name, age in sibling_ages.items():
+    if (age >= 40):
+        print(f'{name} is a senior.') #Prints two times; each person older than 40.
+    else:
+        print(f'{name} is just an adult.') #Prints five times; each person younger than 40.
+
+print(fav_colours[-2:]) # ['Red', 'Orange']
+
+family_ages = [age + 1 for age in family_ages]
+print(family_ages) # [65, 64, 39, 38, 33, 31, 1]
+
+fav_colours.extend(['Cobalt', 'Silver']) #Adds Cobalt and Silver to the end of the list.
