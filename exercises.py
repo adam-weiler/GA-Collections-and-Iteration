87fc6db6722883249a47d1463a043020c05dd2e3
adam-weiler <adam.weiler@yahoo.ca>
2019-07-11 14:02:26 -0400

    Exercise 0 complete, lists and declarations declared.

---
+++ exercises.py
@@ -0,0 +1,13 @@
+#Exercise 0
+
+#Lists
+fav_colours = ['blue', 'black', 'red', 'orange']
+family_ages = [32, 38, 37, 30, 64, 63]
+coin_flip = ['heads', 'tails', 'heads', 'heads', 'tails']
+fav_artists = ['Nightwish', 'Pink Floyd', 'Hawkwind', 'Kamelot', 'Tool']
+
+#Dictionaries
+unique_words = { 'polypod': 'a many-legged animal', 'gamophobia': 'the fear of marriage', 'logodaedalus': 'an artificer in words' }
+fav_movies = { 'Ex Machina': 2014, 'Back to the Future':1985, 'Predestination':2014 }
+pop_cities = { 'Boston': 685100, 'Toronto':2930000, 'Halifax':431500, 'Hamburg':1810000 }
+sibling_ages = { 'Lucas':38, 'Erin':37, 'Kathleen':30, 'Justin':30, 'Patrick':31 }
