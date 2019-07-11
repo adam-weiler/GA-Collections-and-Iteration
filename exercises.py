5ab2012e20568adb6f4d475d8c3c200a10401895
adam-weiler <adam.weiler@yahoo.ca>
2019-07-11 14:55:21 -0400

    Exercise 4 complete.

--- exercises.py
+++ exercises.py
@@ -25,7 +25,7 @@ print (fav_movies['Predestination']) # 2014
 #Exercise 2
 print('\nExercise #2')
 print (fav_colours[-1]) # orange
-pop_cities['San Francisco'] = 884400
+pop_cities['San Francisco'] = 884400 #Adds a new city to the dictionary.
 coin_flips.reverse() #Reverses the order of the list.
 print(pop_cities['Toronto']) # 2930000
 
@@ -43,3 +43,16 @@ for movie, year in fav_movies.items():
 family_ages.sort(reverse = True) #Sorts from smallest to biggest, then reverses the order.
 
 fav_movies['Beauty and the Beast'] = [1991, 2017] #Adds to the end of the dictionary.
+
+
+#Exercise 4
+print('\nExercise #4')
+
+for age in family_ages:
+    if (age < 40):
+        print(age) #Prints five times; each age less than 40.
+
+print(max(family_ages)) # 64
+coin_flips.count('heads') # 3
+fav_artists.pop(3) #Removes 'Kamelot' from the list.
+pop_cities['Toronto'] = 101101101 #Changes the value in the dictionary.
